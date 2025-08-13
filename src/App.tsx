e9e8a6f1086116de91a3b46ef8da774bf3962c94
Omar Sanad <omarsanad.me@gmail.com>
2025-08-14 00:35:12 +0300

    feat: set up app routing with default redirect and services page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Services from "./page/Services";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/services" />} />
+      <Route path="/services" element={<Services />} />
+    </Routes>
+  );
 };
 
 export default App;
