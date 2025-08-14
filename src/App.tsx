26e87d25f816b1944120c7bdaec062f187651349
Omar Sanad <omarsanad.me@gmail.com>
2025-08-14 18:25:36 +0300

    chore: set up React Router with services and service details routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Services from "./page/Services";
+import ServiceDetails from "./page/ServiceDetails";
+import Navbar from "./components/nav/Navbar";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/services" />} />
-      <Route path="/services" element={<Services />} />
-    </Routes>
+    <div className="">
+      <Navbar />
+      <div className="pt-16">
+        <Routes>
+          <Route path="/" element={<Navigate to="/services" />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/services/:id" element={<ServiceDetails />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
