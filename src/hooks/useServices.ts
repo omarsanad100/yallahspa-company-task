39c239c2c93bbe4dce94a4269d220f31ea0285dd
Omar Sanad <omarsanad.me@gmail.com>
2025-08-14 18:28:52 +0300

    feat: add React Query hooks for fetching services and service by ID

--- src/hooks/useServices.ts
+++ src/hooks/useServices.ts
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchServices, fetchServiceTimes } from "../api/servicesApi";
+import { fetchServices, fetchServiceById } from "../api/servicesApi";
 
 export const useServices = () =>
   useQuery({ queryKey: ["services"], queryFn: fetchServices });
 
-export const useServiceTimes = (id: string) =>
+export const useServiceById = (id: string) =>
   useQuery({
-    queryKey: ["serviceTimes", id],
-    queryFn: () => fetchServiceTimes(id),
+    queryKey: ["service", id],
+    queryFn: () => fetchServiceById(id),
     enabled: !!id,
   });
